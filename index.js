cfe90f900da6f5a6e58c9a5b289dcd99820c153e
SolnViktor <solnviktor@gmail.com>
2020-06-24 11:23:26 +0300

    update

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ let smtp_password = process.env.SMTP_PASSWORD || "---";
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: smtp_login, // generated ethereal user
-        pass: smtp_password, // generated ethereal password
+        user: "solnseviktor@gmail.com", // generated ethereal user
+        pass: "solnsev1", // generated ethereal password
     }, tls: {
         rejectUnauthorized: false
     }
@@ -34,16 +34,17 @@ const {name, contacts, message} = req.body
         to: "solnseviktor@gmail.com", // list of receivers
         subject: "От работодателя", // Subject line
         // text: "Привет", // plain text body
-        html: `<b>Сообщение из портфолио</b>
-<div>Имя: ${name}</div>
-<div>Контакты: ${contacts}</div>
-        <div>Сообщение: ${message}</div>`, // html body
+        html: `
+       <b>Сообщение из портфолио</b>
+       <div>Имя: ${name}</div>
+       <div>Контакты: ${contacts}</div>
+       <div>Сообщение: ${message}</div>`, // html body
     });
     res.send("ok")
 });
 
-let port = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3010;
 
-app.listen(port, function () {
-    console.log('Example app listening on port 3010!');
+app.listen(PORT, function () {
+    console.log(`Example app listening on port: ${PORT}`);
 });
